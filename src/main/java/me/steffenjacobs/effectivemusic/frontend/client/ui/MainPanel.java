11f00a9c271b72d05ef4de964f53c4abaa798176
Steffen Jacobs <steffen.jacobs@hotmail.de>
2018-08-24 23:29:01 +0200

    extracted ControlPanel from MainPanel

--- src/main/java/me/steffenjacobs/effectivemusic/frontend/client/ui/MainPanel.java
+++ src/main/java/me/steffenjacobs/effectivemusic/frontend/client/ui/MainPanel.java
@@ -1,44 +1,26 @@
 package me.steffenjacobs.effectivemusic.frontend.client.ui;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.NativeEvent;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.Response;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanCodex;
 
-import me.steffenjacobs.effectivemusic.frontend.client.controller.Base64Encoder;
 import me.steffenjacobs.effectivemusic.frontend.client.controller.MusicAutobeanFactory;
-import me.steffenjacobs.effectivemusic.frontend.client.event.NextEvent;
-import me.steffenjacobs.effectivemusic.frontend.client.event.PauseMusicEvent;
-import me.steffenjacobs.effectivemusic.frontend.client.event.PreviousEvent;
-import me.steffenjacobs.effectivemusic.frontend.client.event.ResumeMusicEvent;
-import me.steffenjacobs.effectivemusic.frontend.client.event.StartMusicEvent;
-import me.steffenjacobs.effectivemusic.frontend.client.event.StopMusicEvent;
-import me.steffenjacobs.effectivemusic.frontend.client.event.TrackPositionChangeEvent;
-import me.steffenjacobs.effectivemusic.frontend.client.event.VolumeChangeEvent;
 import me.steffenjacobs.effectivemusic.frontend.client.event.refresh.RefreshPlayerInformationEvent;
 import me.steffenjacobs.effectivemusic.frontend.client.event.refresh.RefreshPlaylistInformationEvent;
 import me.steffenjacobs.effectivemusic.frontend.client.event.refresh.RefreshTrackInformationEvent;
-import me.steffenjacobs.effectivemusic.frontend.client.resource.Messages;
-import me.steffenjacobs.effectivemusic.frontend.client.ui.component.FormattingUtils;
+import me.steffenjacobs.effectivemusic.frontend.client.ui.component.controlpanel.ControlPanel;
 import me.steffenjacobs.effectivemusic.frontend.client.ui.component.playlist.EffectiveMusicResources;
 import me.steffenjacobs.effectivemusic.frontend.client.ui.component.playlist.PlaylistManager;
 import me.steffenjacobs.effectivemusic.frontend.client.ui.component.playlist.PlaylistPanel;
-import me.steffenjacobs.effectivemusic.frontend.client.ui.component.simpleslider.SimpleSlider;
-import me.steffenjacobs.effectivemusic.frontend.client.ui.component.simpleslider.SliderEventHandler;
 import me.steffenjacobs.effectivemusic.frontend.common.domain.PlayerInformationDTO;
 import me.steffenjacobs.effectivemusic.frontend.common.domain.PlaylistDto;
 import me.steffenjacobs.effectivemusic.frontend.common.domain.TrackDto;
@@ -49,48 +31,16 @@ public class MainPanel extends Composite {
 	private static MainPanelUiBinder uiBinder = GWT.create(MainPanelUiBinder.class);
 	private static MusicAutobeanFactory factory = GWT.create(MusicAutobeanFactory.class);
 
-	private static Messages msg = GWT.create(Messages.class);
-
 	interface MainPanelUiBinder extends UiBinder<Widget, MainPanel> {
 	}
 
 	@UiField
-	Button stopButton;
-
-	@UiField
-	Button startPauseButton;
-
-	@UiField
-	Button nextButton;
-
-	@UiField
-	Button previousButton;
-
-	@UiField
-	TextBox textBox;
-
-	@UiField
-	Label playTime;
-
-	@UiField
-	Label playTitle;
-
-	@UiField
-	Label playVolume;
-
-	@UiField
-	SimpleSlider sliderVolumeUi;
-
-	@UiField
-	SimpleSlider sliderTrackUi;
+	ControlPanel controlPanel;
 
 	@UiField
 	PlaylistPanel playlistPanelUi;
 
 	private SimpleEventBus eventBus;
-
-	private boolean playing = false;
-	private boolean paused = false;
 
 	private final PlaylistManager playlistManager;
 
@@ -102,37 +52,31 @@ public class MainPanel extends Composite {
 		public void run() {
 			eventBus.fireEvent(new RefreshTrackInformationEvent(new DefaultRequestCallback() {
 
-				private void clearTextFields() {
-					playTitle.setText("No Track");
-					playTime.setText("");
-					playVolume.setText("");
-				}
-
 				@Override
 				public void onResponseReceived(Request request, Response response) {
 					if ("".equals(response.getText())) {
-						clearTextFields();
+						controlPanel.clearTextFields();
 						return;
 					} else {
 						AutoBean<TrackDto> bean = AutoBeanCodex.decode(factory, TrackDto.class, response.getText());
 						final TrackDto autoBeanTrack = bean.as();
 						TrackDto dto = new TrackImpl(autoBeanTrack);
 						if (dto.equals(lastTrack)) {
-							setPosition(dto.getPosition(), dto.getLength());
+							controlPanel.setPosition(dto.getPosition(), dto.getLength());
 							return;
 						}
 						playlistManager.setCurrentTrack(dto);
 						lastTrack = dto;
 						if (autoBeanTrack == null) {
-							clearTextFields();
+							controlPanel.clearTextFields();
 							return;
 						}
 						if (dto.getTitle() == null) {
-							playTitle.setText("No Track");
+							controlPanel.setTrackTitle("No Track");
 						} else {
-							playTitle.setText(dto.getTitle() + " - " + dto.getArtist());
+							controlPanel.setTrackTitle(dto.getTitle() + " - " + dto.getArtist());
 						}
-						setPosition(dto.getPosition(), dto.getLength());
+						controlPanel.setPosition(dto.getPosition(), dto.getLength());
 					}
 				}
 			}));
@@ -152,69 +96,15 @@ public class MainPanel extends Composite {
 				public void onResponseReceived(Request request, Response response) {
 					AutoBean<PlayerInformationDTO> bean = AutoBeanCodex.decode(factory, PlayerInformationDTO.class, response.getText());
 					PlayerInformationDTO dto = bean.as();
-					setPlaying("PLAYING".equals(dto.getStatus()));
-					setVolume(dto.getVolume());
+					controlPanel.setPlaying("PLAYING".equals(dto.getStatus()));
+					controlPanel.setVolume(dto.getVolume());
 				}
 			}));
 		}
 	};
 
-	public void setPosition(double position, long length) {
-		playTime.setText(FormattingUtils.formatPosition(position, length) + " - " + FormattingUtils.formatTime(length));
-		sliderTrackUi.setPosition(position * 100);
-	}
-
-	public void setVolume(double volume) {
-		playVolume.setText("Volume: " + FormattingUtils.formatPercent(volume) + "%");
-		sliderVolumeUi.setPosition(volume);
-	}
-
-	public void setPlaying(boolean value) {
-		startPauseButton.setText(value ? msg.pauseButton() : msg.startButton());
-		playing = value;
-	}
-
 	private void startAutoUpdate() {
 		t.scheduleRepeating(1000);
-	}
-
-	private void setupVolumeListener() {
-		sliderVolumeUi.addEventHandler(new SliderEventHandler() {
-			@Override
-			public void onClick(NativeEvent event) {
-				eventBus.fireEvent(new VolumeChangeEvent(sliderVolumeUi.getPosition(), null));
-			}
-
-			@Override
-			public void onDrag(NativeEvent event) {
-				eventBus.fireEvent(new VolumeChangeEvent(sliderVolumeUi.getPosition(), null));
-			}
-
-			@Override
-			public void onDragEnd(NativeEvent event) {
-				eventBus.fireEvent(new VolumeChangeEvent(sliderVolumeUi.getPosition(), null));
-			}
-		});
-	}
-
-	private void setupTrackListener() {
-		sliderTrackUi.addEventHandler(new SliderEventHandler() {
-			@Override
-			public void onClick(NativeEvent event) {
-				eventBus.fireEvent(new TrackPositionChangeEvent(sliderTrackUi.getPosition(), null));
-			}
-
-			@Override
-			public void onDrag(NativeEvent event) {
-				eventBus.fireEvent(new TrackPositionChangeEvent(sliderTrackUi.getPosition(), null));
-			}
-
-			@Override
-			public void onDragEnd(NativeEvent event) {
-				eventBus.fireEvent(new TrackPositionChangeEvent(sliderTrackUi.getPosition(), null));
-			}
-		});
-
 	}
 
 	@Inject
@@ -222,63 +112,10 @@ public class MainPanel extends Composite {
 		eventBus = evtBus;
 		// init display
 		initWidget(uiBinder.createAndBindUi(this));
+		controlPanel.setEventBus(evtBus);
 		EffectiveMusicResources.INSTANCE.style().ensureInjected();
 		playlistManager = new PlaylistManager(playlistPanelUi);
-		textBox.getElement().setPropertyString("placeholder", msg.textboxPlaceholder());
-		setupTrackListener();
-		setupVolumeListener();
 		startAutoUpdate();
 
-	}
-
-	@UiHandler("stopButton")
-	void onStopButtonClicked(ClickEvent e) {
-		eventBus.fireEvent(new StopMusicEvent(new DefaultRequestCallback() {
-			@Override
-			public void onResponseReceived(Request request, Response response) {
-				setPlaying(false);
-				paused = false;
-			}
-		}));
-	}
-
-	@UiHandler("startPauseButton")
-	void onStartPauseButtonClicked(ClickEvent e) {
-		if (playing) {
-			eventBus.fireEvent(new PauseMusicEvent(new DefaultRequestCallback() {
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					setPlaying(false);
-					paused = true;
-				}
-			}));
-		} else if (paused) {
-			eventBus.fireEvent(new ResumeMusicEvent(new DefaultRequestCallback() {
-
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					setPlaying(true);
-					paused = false;
-				}
-			}));
-		} else {
-			eventBus.fireEvent(new StartMusicEvent(new String(Base64Encoder.encode(textBox.getText().getBytes())), new DefaultRequestCallback() {
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					setPlaying(true);
-				}
-			}));
-		}
-
-	}
-
-	@UiHandler("nextButton")
-	void onNextButtonClicked(ClickEvent e) {
-		eventBus.fireEvent(new NextEvent());
-	}
-
-	@UiHandler("previousButton")
-	void onPreviousButtonClicked(ClickEvent e) {
-		eventBus.fireEvent(new PreviousEvent());
 	}
 }
---
+++ src/main/java/me/steffenjacobs/effectivemusic/frontend/client/ui/component/controlpanel/ControlPanel.java
@@ -0,0 +1,205 @@
+package me.steffenjacobs.effectivemusic.frontend.client.ui.component.controlpanel;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+import me.steffenjacobs.effectivemusic.frontend.client.controller.Base64Encoder;
+import me.steffenjacobs.effectivemusic.frontend.client.event.NextEvent;
+import me.steffenjacobs.effectivemusic.frontend.client.event.PauseMusicEvent;
+import me.steffenjacobs.effectivemusic.frontend.client.event.PreviousEvent;
+import me.steffenjacobs.effectivemusic.frontend.client.event.ResumeMusicEvent;
+import me.steffenjacobs.effectivemusic.frontend.client.event.StartMusicEvent;
+import me.steffenjacobs.effectivemusic.frontend.client.event.StopMusicEvent;
+import me.steffenjacobs.effectivemusic.frontend.client.event.TrackPositionChangeEvent;
+import me.steffenjacobs.effectivemusic.frontend.client.event.VolumeChangeEvent;
+import me.steffenjacobs.effectivemusic.frontend.client.resource.Messages;
+import me.steffenjacobs.effectivemusic.frontend.client.ui.DefaultRequestCallback;
+import me.steffenjacobs.effectivemusic.frontend.client.ui.component.FormattingUtils;
+import me.steffenjacobs.effectivemusic.frontend.client.ui.component.simpleslider.SimpleSlider;
+import me.steffenjacobs.effectivemusic.frontend.client.ui.component.simpleslider.SliderEventHandler;
+
+/** @author Steffen Jacobs */
+public class ControlPanel extends Composite {
+
+	private static ControlPanelUiBinder uiBinder = GWT.create(ControlPanelUiBinder.class);
+
+	interface ControlPanelUiBinder extends UiBinder<Widget, ControlPanel> {
+	}
+
+	private static Messages msg = GWT.create(Messages.class);
+
+	@UiField
+	Button stopButton;
+
+	@UiField
+	Button startPauseButton;
+
+	@UiField
+	Button nextButton;
+
+	@UiField
+	Button previousButton;
+
+	@UiField
+	TextBox textBox;
+
+	@UiField
+	Label playTime;
+
+	@UiField
+	Label playTitle;
+
+	@UiField
+	Label playVolume;
+
+	@UiField
+	SimpleSlider sliderVolumeUi;
+
+	@UiField
+	SimpleSlider sliderTrackUi;
+
+	SimpleEventBus eventBus;
+
+	private boolean playing = false;
+	private boolean paused = false;
+
+	public ControlPanel() {
+		GWT.log("initing control panel");
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	public void setEventBus(SimpleEventBus evtBus) {
+		this.eventBus = evtBus;
+		textBox.getElement().setPropertyString("placeholder", msg.textboxPlaceholder());
+		setupTrackListener();
+		setupVolumeListener();
+	}
+
+	public void setPosition(double position, long length) {
+		playTime.setText(FormattingUtils.formatPosition(position, length) + " - " + FormattingUtils.formatTime(length));
+		sliderTrackUi.setPosition(position * 100);
+	}
+
+	public void setVolume(double volume) {
+		playVolume.setText("Volume: " + FormattingUtils.formatPercent(volume) + "%");
+		sliderVolumeUi.setPosition(volume);
+	}
+
+	public void setPlaying(boolean value) {
+		startPauseButton.setText(value ? msg.pauseButton() : msg.startButton());
+		playing = value;
+	}
+
+	private void setupVolumeListener() {
+		sliderVolumeUi.addEventHandler(new SliderEventHandler() {
+			@Override
+			public void onClick(NativeEvent event) {
+				eventBus.fireEvent(new VolumeChangeEvent(sliderVolumeUi.getPosition(), null));
+			}
+
+			@Override
+			public void onDrag(NativeEvent event) {
+				eventBus.fireEvent(new VolumeChangeEvent(sliderVolumeUi.getPosition(), null));
+			}
+
+			@Override
+			public void onDragEnd(NativeEvent event) {
+				eventBus.fireEvent(new VolumeChangeEvent(sliderVolumeUi.getPosition(), null));
+			}
+		});
+	}
+
+	private void setupTrackListener() {
+		sliderTrackUi.addEventHandler(new SliderEventHandler() {
+			@Override
+			public void onClick(NativeEvent event) {
+				eventBus.fireEvent(new TrackPositionChangeEvent(sliderTrackUi.getPosition(), null));
+			}
+
+			@Override
+			public void onDrag(NativeEvent event) {
+				eventBus.fireEvent(new TrackPositionChangeEvent(sliderTrackUi.getPosition(), null));
+			}
+
+			@Override
+			public void onDragEnd(NativeEvent event) {
+				eventBus.fireEvent(new TrackPositionChangeEvent(sliderTrackUi.getPosition(), null));
+			}
+		});
+
+	}
+
+	@UiHandler("stopButton")
+	void onStopButtonClicked(ClickEvent e) {
+		eventBus.fireEvent(new StopMusicEvent(new DefaultRequestCallback() {
+			@Override
+			public void onResponseReceived(Request request, Response response) {
+				setPlaying(false);
+				paused = false;
+			}
+		}));
+	}
+
+	@UiHandler("startPauseButton")
+	void onStartPauseButtonClicked(ClickEvent e) {
+		if (playing) {
+			eventBus.fireEvent(new PauseMusicEvent(new DefaultRequestCallback() {
+				@Override
+				public void onResponseReceived(Request request, Response response) {
+					setPlaying(false);
+					paused = true;
+				}
+			}));
+		} else if (paused) {
+			eventBus.fireEvent(new ResumeMusicEvent(new DefaultRequestCallback() {
+
+				@Override
+				public void onResponseReceived(Request request, Response response) {
+					setPlaying(true);
+					paused = false;
+				}
+			}));
+		} else {
+			eventBus.fireEvent(new StartMusicEvent(new String(Base64Encoder.encode(textBox.getText().getBytes())), new DefaultRequestCallback() {
+				@Override
+				public void onResponseReceived(Request request, Response response) {
+					setPlaying(true);
+				}
+			}));
+		}
+
+	}
+
+	@UiHandler("nextButton")
+	void onNextButtonClicked(ClickEvent e) {
+		eventBus.fireEvent(new NextEvent());
+	}
+
+	@UiHandler("previousButton")
+	void onPreviousButtonClicked(ClickEvent e) {
+		eventBus.fireEvent(new PreviousEvent());
+	}
+
+	public void clearTextFields() {
+		playTitle.setText("No Track");
+		playTime.setText("");
+		playVolume.setText("");
+	}
+
+	public void setTrackTitle(String trackTitle) {
+		playTitle.setText(trackTitle);
+	}
+
+}
